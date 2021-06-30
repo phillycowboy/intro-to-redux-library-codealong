57cf9124f13439e31996f9a457ca46c6a818007a
Ben Harter-Murphy <benhartermurphy@gmail.com>
2021-06-30 17:56:57 -0400

    Done.

--- src/reducers/shoppingListItemReducer.js
+++ src/reducers/shoppingListItemReducer.js
@@ -4,13 +4,17 @@ export default function shoppingListItemReducer(
 	},
 	action
 ) {
+	console.log(action)
 	switch (action.type) {
 		case 'INCREASE_COUNT':
+			console.log("Current state.items.length", state.items.length)
+			console.log("Updating state.items.length", state.items.length + 1)
 			return {
 				...state,
 				items: state.items.concat(state.items.length + 1)
 			}
 		default:
+			console.log("Initial state.items.length", state.items.length)
 			return state;
 	}
 }
